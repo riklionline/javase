f081051282d1f8516b57ee1ed51b4c0c7883f3f9
riklionline <rik.li@hotmail.com>
2024-05-05 21:43:59 +0800

    first commit.

---
+++ src/com/riklionline/dualcoloredball/DualColoredBall.java
@@ -0,0 +1,12 @@
+package src.com.riklionline.dualcoloredball;
+
+public class DualColoredBall {
+
+    public static void main(String[] args) {
+        int[] a = {2,3,4,5,6,7,8,9,22};//技巧:new int[8].var……(.cast/.arg/.par……)
+        System.out.println(a.length);
+        System.out.println(a[8]);
+
+    }
+
+}
