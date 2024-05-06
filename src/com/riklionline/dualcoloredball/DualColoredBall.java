58bf923d2de12a62fd0d3ab6c6ebef285c074670
riklionline <rik.li@hotmail.com>
2024-05-06 13:18:25 +0800

    second commit.

--- src/com/riklionline/dualcoloredball/DualColoredBall.java
+++ src/com/riklionline/dualcoloredball/DualColoredBall.java
@@ -1,11 +1,17 @@
 package src.com.riklionline.dualcoloredball;
 
+/**
+ * @ClassName: DualColoredBall
+ * @Descirption:
+ * @Version: V1.01
+ * @Author: RikLi
+ * @DateTime: 5/6/24 13:14
+ * @Signature: “闲庭书阁飞玉箫，Coding诗酒醉年华。”
+ */
 public class DualColoredBall {
 
+    //Main Method
     public static void main(String[] args) {
-        int[] a = {2,3,4,5,6,7,8,9,22};//技巧:new int[8].var……(.cast/.arg/.par……)
-        System.out.println(a.length);
-        System.out.println(a[8]);
 
     }
 
