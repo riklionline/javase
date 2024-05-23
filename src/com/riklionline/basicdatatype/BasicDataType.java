2b06c9e8e24da72bd6d1559a6e60f4a71c1d5391
riklionline <rik.li@hotmail.com>
2024-05-23 09:34:43 +0800

    BasicDataType case coding complete.

---
+++ src/com/riklionline/basicdatatype/BasicDataType.java
@@ -0,0 +1,19 @@
+package src.com.riklionline.basicdatatype;
+
+/**
+ * @ClassName: BasicDataType
+ * @Descirption:
+ * @Version: v1.01
+ * @Author: RikLi
+ * @DateTime: 5/23/24 09:15
+ * @Signature: “闲庭书阁飞玉箫，Coding诗酒醉年华。”
+ */
+public class BasicDataType {
+
+    public static void main(String[] args) {
+
+        //char: 0~65535, jvm默认使用Unicode编码格式，1char = 2Byte，在C语言中，char的范围是（-128~128）
+        char a = 100;
+        System.out.println(a);
+    }
+}
