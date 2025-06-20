8efc503a044f76d7fb78f3955968b2741efaabf5
rikli <rik.li@hotmail.com>
2025-06-20 17:00:03 +0800

    Daily coding complete.

--- src/com/riklionline/bean/Person.java
+++ src/com/riklionline/bean/Person.java
@@ -1,5 +1,6 @@
 package com.riklionline.bean;
 
+import java.io.Serializable;
 import java.util.Objects;
 
 /**
@@ -10,7 +11,7 @@ import java.util.Objects;
  * @DateTime: 2025/6/7 1:22 PM
  * @Signature: “闲庭书阁飞玉箫，Coding诗酒醉年华。”
  */
-public class Person {
+public class Person implements Serializable {
 
     private String name;
     private int age;
