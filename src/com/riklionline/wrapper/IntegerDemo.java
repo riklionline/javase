4b5f3fd5c6e9f90e44ab9ca17b329698ec940e12
rikli <rik.li@hotmail.com>
2025-06-27 13:07:01 +0800

    Daily coding complete.

---
+++ src/com/riklionline/wrapper/IntegerDemo.java
@@ -0,0 +1,44 @@
+package com.riklionline.wrapper;
+
+/**
+ * 原始类型	包装类
+ * byte	    Byte
+ * boolean	Boolean
+ * char	    Character
+ * double	Double
+ * float	Float
+ * int	    Integer
+ * long	    Long
+ * short	Short
+ * -------------------------------
+ * 为什么要用包装类，因为功能多
+ */
+
+public class IntegerDemo {
+
+    public static void main(String[] args) {
+
+        //Static方法
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        Integer integer1 = new Integer(100);
+        Integer integer2 = new Integer("100");
+
+        int i2 = integer2.intValue();//integer ——> int
+        System.out.println(i2);
+
+        int i3 = Integer.parseInt("100");//String ——> int
+        System.out.println(i3);
+
+        Integer integer3 = Integer.valueOf(100);// int——>integer
+        System.out.println(integer3);
+
+        //十进制 ——> 二进制/十六进制
+        String binaryString = Integer.toBinaryString(100);
+        System.out.println(binaryString);
+        String hexString = Integer.toHexString(100);
+        System.out.println(hexString);
+    }
+
+}
