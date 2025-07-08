100f1fb6a64ef03b7a4a4f5bbad75c4e03355ae0
rikli <rik.li@hotmail.com>
2025-07-08 20:57:47 +0800

    Daily coding complete.

---
+++ src/com/riklionline/reflection/dynamicproxy/ProxyUtil.java
@@ -0,0 +1,15 @@
+package com.riklionline.reflection.dynamicproxy;
+
+import com.riklionline.reflection.dynamicproxy.impl.BigStar;
+
+
+public class ProxyUtil {
+
+    public static Star createProxy(BigStar bigStar) {
+
+
+        Star star = new BigStar();
+        return star;
+    }
+
+}
---
+++ src/com/riklionline/reflection/dynamicproxy/Star.java
@@ -0,0 +1,9 @@
+package com.riklionline.reflection.dynamicproxy;
+
+public interface Star {
+
+    public abstract String sing();
+
+    public abstract void dance();
+
+}
